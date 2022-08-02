268402b071f1527016d95ff488389f5f0d2bab34
Daniel Kunkler <kunkl007@umn.edu>
2022-08-01 19:23:21 -0500

    Simple working version, no rate limiter

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const port = 3000;
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Success!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
